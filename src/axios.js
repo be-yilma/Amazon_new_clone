5730d7ab125dbbe1c555d79bee3ee5fab40069dd
bezuayehu <bezuayehu87@gmail.com>
2024-01-15 20:30:45 -0500

    axios update

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:4242",
+  // baseURL: "http://localhost:4242",
+  baseURL: "https://buzu-amazon.cyclic.app/",
 });
 
 export default instance;
